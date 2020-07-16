940f4e73538aa3d352ec5f68dff2eed74916493e
SADIK Brahim <62760511+sadikBr@users.noreply.github.com>
2020-07-16 01:21:00 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const display = document.body;
+
+setInterval(() => {
+  for (let i = 0; i < 2; i++) {
+    const snowFlake = document.createElement("i");
+    snowFlake.classList.add("fas");
+    snowFlake.classList.add("fa-snowflake");
+    snowFlake.style.left = Math.random() * window.innerWidth + "px";
+    snowFlake.style.opacity = 0.3 + Math.random();
+    snowFlake.style.fontSize = 7 + Math.random() * 10 + "px";
+
+    display.appendChild(snowFlake);
+    snowFlake.addEventListener("animationend", () => {
+      snowFlake.remove();
+    });
+  }
+}, 25);
